b01dbcc58e41f6161631066c6d261b542ae5bf37
DestineePW <95252380+DestineePW@users.noreply.github.com>
2021-12-17 16:37:32 -0600

    Modal and Backdrop Working DeleteHandler function

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -9,6 +9,11 @@ function Todo(props) {
   function deleteHandler() {
     setModalIsOpen(true);
   }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -18,7 +23,7 @@ function Todo(props) {
         </button>
       </div>
       {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
