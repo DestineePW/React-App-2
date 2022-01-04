736e606512fea7799a1e39d52339c043ffa1bdef
DestineePW <95252380+DestineePW@users.noreply.github.com>
2022-01-04 17:32:59 -0600

    I don't know man I added a lot of stuff lost track

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./Pages/AllMeetups";
+import NewMeetupPage from "./Pages/NewMeetups";
+import FavoritesPage from "./Pages/Favorites";
+
 function App() {
-  return <div></div>;
+  //Example of path
+  // Localhost:300/path
+  return (
+    <Routes>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+    </Routes>
+    
+  );
 }
 
 export default App;
